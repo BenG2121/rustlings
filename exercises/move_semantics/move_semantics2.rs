d8c79aefc97903b56cd04f23049505d5fe2fdf4d
Calypso <benjamingoettlich@gmx.de>
2022-08-15 09:40:32 +0000

    Solve move_semantics

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,10 +2,10 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
     let mut vec1 = fill_vec(&mut vec0);
 
@@ -18,11 +18,12 @@ fn main() {
 }
 
 fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+    let vec2;
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    //vec
+    vec2 = vec;
+    vec2.to_vec()
 }
