0a48cbc5156cabac7a560b376134f6b4d45d2e8d
Calypso <benjamingoettlich@gmx.de>
2022-08-16 07:19:24 +0000

    Solve enums3

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,7 +2,7 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
@@ -43,7 +43,10 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
        match message {
-         self.color = message,af
+         Message::ChangeColor(color) => self.change_color(color),
+         Message::Echo(echo) => self.echo(echo),
+         Message::Move(point) => self.move_position(point),
+         Message::Quit => self.quit(),         
        }
     }
 }
