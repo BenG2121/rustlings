2b01a8a4e65209d043ca999b7a20f19832c71f1b
Benjamin Goettlich <benjamingoettlich@gmx.de>
2022-08-17 08:04:44 +0200

    Solve hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,12 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
+#[derive(Debug)]
 struct Team {
     name: String,
     goals_scored: u8,
@@ -40,7 +41,30 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let winner = scores.entry(team_1_name).or_insert(
+            Team {
+            name: "team".to_string(),
+            goals_scored: 0,
+            goals_conceded: 0,
+            }
+        );
+
+        (*winner).goals_scored += team_1_score;
+        (*winner).goals_conceded += team_2_score;
+
+        let loser = scores.entry(team_2_name).or_insert(Team {
+            name: "team".to_string(),
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        (*loser).goals_conceded += team_1_score;
+        (*loser).goals_scored += team_2_score;
     }
+
+    for (key, value) in &scores {
+        println!("{}: {:?}", key, value);
+    }
+
     scores
 }
 
